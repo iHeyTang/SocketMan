fd81e115f0a3f454f525b657c5e7e34026e5ad60
iHeyTang <dehui1012@qq.com>
2024-07-11 19:12:09 +0800

    feat: 在最底端或内容没有充满屏幕时进行自动滚动

--- src/App.vue
+++ src/App.vue
@@ -145,7 +145,8 @@ const handleStartServer = (id: string) => {
     if (!inst) return
     inst.logs.push({ type: 'message', messageType: 'receive', clientKey: key, message })
     const logsContainer = document.getElementById('logsContainer');
-    if (logsContainer && logsContainer.scrollTop === logsContainer.scrollHeight) {
+    // 在最底端或内容没有充满屏幕时进行自动滚动
+    if (logsContainer && (logsContainer.scrollHeight === logsContainer.scrollTop || logsContainer.scrollHeight - logsContainer.scrollTop < logsContainer.clientHeight + 100)) {
       setTimeout(() => {
         logsContainer.scrollTop = logsContainer.scrollHeight;
       }, 0)
