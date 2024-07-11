507123268aa81780d740292324efbb074b13bd9d
iHeyTang <dehui1012@qq.com>
2024-07-12 00:49:16 +0800

    fix: 修复版本脚本

--- scripts/version.js
+++ scripts/version.js
@@ -11,26 +11,26 @@ if (status) {
 /**
  * @typedef {'minor' | 'major' | 'patch'} releaseType
  */
-const releaseType = process.argv[0];
+const releaseType = process.argv[2];
 if (!['minor', 'major', 'patch'].includes(releaseType)) {
   console.error('Invalid version release type');
   process.exit(1);
 }
 
-const utoolsPluginJsonPath = '../plugin.json';
+const utoolsPluginJsonPath = './plugin.json';
 const utoolsPluginJsonContent = fs.readFileSync(utoolsPluginJsonPath, 'utf8');
 const utoolsPluginJson = JSON.parse(utoolsPluginJsonContent);
 const currentUtoolsPluginVersion = utoolsPluginJson.version;
 
 // 读取package.json文件
-const packageJsonPath = '../package.json';
+const packageJsonPath = './package.json';
 const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
 const packageJson = JSON.parse(packageJsonContent);
 const currentPackageJson = packageJson.version;
 
 if (currentUtoolsPluginVersion !== currentPackageJson) {
   console.error('utools plugin version is not equal to package.json version');
-  process.exec(1);
+  process.exit(1);
 }
 
 const newVersion = semver.inc(currentPackageJson, releaseType);
@@ -43,3 +43,8 @@ fs.writeFileSync(utoolsPluginJsonPath, newUtoolsPluginJsonContent, 'utf8');
 
 const newPackageJsonContent = JSON.stringify(packageJson, null, 2);
 fs.writeFileSync(packageJsonPath, newPackageJsonContent, 'utf8');
+
+execSync('git add plugin.json package.json');
+execSync(`git commit -m "version: release v${newVersion}"`);
+execSync(`git tag v${newVersion}`);
+execSync('git push --tags');
